f74a83bae78d4311519e7e4e1204fb0c9e57e23f
Jiří Strnadel <Juzba88@gmail.com>
2025-05-16 20:20:48 +0200

    Signal IR

--- OnlineChat_Blazor/Program.cs
+++ OnlineChat_Blazor/Program.cs
@@ -1,10 +1,21 @@
 using OnlineChat_Blazor.Components;
+using OnlineChat_Blazor.Utilities;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+ builder.Services.AddSignalR(); // Pøidání SignalR
+
+
+
+
+
+
+
+
 
 var app = builder.Build();
 
@@ -24,5 +35,7 @@ app.UseAntiforgery();
 app.MapStaticAssets();
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
+
+app.MapHub<ChatHub>("/chathub");
 
 app.Run();
---
+++ OnlineChat_Blazor/Utilities/ChatHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace OnlineChat_Blazor.Utilities
+{
+    public class ChatHub: Hub
+    {
+        public async Task SendMessage()
+        {
+            await Clients.All.SendAsync("RefreshChat");
+        }
+    }
+}
