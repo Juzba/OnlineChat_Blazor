6fd08ec5b8bb14855c2b8a0fa908b2cbc52fb04d
Jiří Strnadel <Juzba88@gmail.com>
2025-05-19 01:08:45 +0200

    Signal ir done and db online check

---
+++ OnlineChat_Blazor/Utilities/OpenDB.cs
@@ -0,0 +1,57 @@
+﻿using OnlineChat_Blazor.Models;
+
+namespace OnlineChat_Blazor.Utilities;
+
+public static class OpenDB
+{
+
+    public static void LoadDB(ref List<Message>? messages, ref List<User>? users)
+    {
+
+        try
+        {
+            using (var context = new MyDBContext())
+            {
+                messages = context.Messages.OrderDescending().Take(10).ToList();
+                users = context.Users.ToList();
+            }
+        }
+        catch (Exception ex)
+        {
+            messages = null;
+            users = null;
+
+            Console.BackgroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine($"\nChyba připojení k DB: {ex.Message} \n");
+            Console.Beep();
+        }
+    }
+
+
+    public static bool SaveDB(Message message)
+    {
+        try
+        {
+            using (var context = new MyDBContext())
+            {
+                context.Messages.Add(message);
+                context.SaveChanges();
+                return true;
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.BackgroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine($"\nChyba připojení k DB: {ex.Message} \n");
+            Console.Beep();
+            return false;
+        }
+    }
+
+
+
+
+
+
+}
+
